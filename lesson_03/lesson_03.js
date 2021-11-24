c67f0d41e46dbed2f18473ab147e960486ff1165
slavapapov <slava@papov.tech>
2021-11-25 02:24:04 +0300

    env setup before lesson 04

--- lesson_03/lesson_03.js
+++ lesson_03/lesson_03.js
@@ -5,20 +5,26 @@
 “%ip-адрес%_requests.log”.
 */
 
-const readline = require('readline');
-const fs = require('fs');
-const srcFile = './access.log';
+const readline = require('readline')
+const fs = require('fs')
+const srcFile = './access.log'
 const _ips = ['89.123.1.41', '34.48.240.111']
-const targets = Object.fromEntries(_ips.map(ip => [ip, fs.createWriteStream(`./${ip}_requests.log`) ] ));
-const nl = '\n';
+const targets = Object.fromEntries(
+  _ips.map((ip) => [ip, fs.createWriteStream(`./${ip}_requests.log`)])
+)
+const nl = '\n'
 
-const readlineInterface = readline.createInterface({ input: fs.createReadStream(srcFile) });
+const readlineInterface = readline.createInterface({
+  input: fs.createReadStream(srcFile),
+})
 readlineInterface.on('line', (line) => {
   for (const [ip, ws] of Object.entries(targets)) {
-    if (line.startsWith(ip)) { ws.write(line + nl); }
+    if (line.startsWith(ip)) {
+      ws.write(line + nl)
+    }
   }
-});
+})
 
 readlineInterface.on('close', (line) => {
-  Object.values(targets).forEach(ws => ws.close());
-});
+  Object.values(targets).forEach((ws) => ws.close())
+})
